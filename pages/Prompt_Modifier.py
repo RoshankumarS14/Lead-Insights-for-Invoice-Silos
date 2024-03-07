1ed8ae8f3e13aa49f3a6aa525385c1d959de8608
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-03-08 00:06:04 +0530

    Update Prompt_Modifier.py

--- pages/Prompt_Modifier.py
+++ pages/Prompt_Modifier.py
@@ -1,28 +1,33 @@
 import streamlit as st
 
-# Read the prompts from the file
-with open('prompts.txt', 'r') as file:
-    prompts = file.read().split('---')
 
-quality_prompt = prompts[0].strip()
-quantity_prompt = prompts[1].strip()
+if st.session_state["authentication_status"]:
+    # Read the prompts from the file
+    with open('prompts.txt', 'r') as file:
+        prompts = file.read().split('---')
+    
+    quality_prompt = prompts[0].strip()
+    quantity_prompt = prompts[1].strip()
+    
+    # Display the text areas for the prompts
+    st.subheader("Quality Prompt")
+    quality_prompt = st.text_area("", quality_prompt,height=400)
+    save_quality = st.button('Save Quality Prompt')
+    # Display the buttons to save the prompts
+    if save_quality:
+        prompts[0] = quality_prompt
+        with open('prompts.txt', 'w') as file:
+            file.write('---'.join(prompts))
+        st.success('Quality prompt saved!')
+    
+    st.subheader("Quantity Prompt")
+    quantity_prompt = st.text_area("", quantity_prompt,height=440)
+    save_quantity = st.button('Save Quantity Prompt')
+    if save_quantity:
+        prompts[1] = quantity_prompt
+        with open('prompts.txt', 'w') as file:
+            file.write('---'.join(prompts))
+        st.success('Quantity prompt saved!')
 
-# Display the text areas for the prompts
-st.subheader("Quality Prompt")
-quality_prompt = st.text_area("", quality_prompt,height=400)
-save_quality = st.button('Save Quality Prompt')
-# Display the buttons to save the prompts
-if save_quality:
-    prompts[0] = quality_prompt
-    with open('prompts.txt', 'w') as file:
-        file.write('---'.join(prompts))
-    st.success('Quality prompt saved!')
-
-st.subheader("Quantity Prompt")
-quantity_prompt = st.text_area("", quantity_prompt,height=440)
-save_quantity = st.button('Save Quantity Prompt')
-if save_quantity:
-    prompts[1] = quantity_prompt
-    with open('prompts.txt', 'w') as file:
-        file.write('---'.join(prompts))
-    st.success('Quantity prompt saved!')
+else:
+    st.warning("Login first!")
